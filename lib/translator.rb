84cd915f00102310ff24841aaa4fc086216da894
Jacky Lin <jacky.jia.lin@gmail.com>
2020-02-16 07:21:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,14 +1,7 @@
 # require modules here
 require "yaml"
 def load_library
-hash = YAML.load_file(path)
-  get_meaning = {}
-  get_emoticon = {}
-  hash.each do |key, value|
-    get_meaning[value[1]] = key
-    get_emoticon[value[0]] = value[1]
-  end
-  return {'get_meaning'=> get_meaning, 'get_emoticon'=> get_emoticon}
+
 end
 
 def get_japanese_emoticon
